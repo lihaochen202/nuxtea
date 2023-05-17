39bc9a2809008e8e8aa04fd5261a8839cfbda2ad
lihaochen <lihaochen202@outlook.com>
2023-05-17 13:36:38 +0800

    feat: add pinia

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+const countStore = useCountStore()
+</script>
+
 <template>
   <div>
+    <div @click="countStore.increment">
+      count: {{ countStore.count }}, doubleCount: {{ countStore.doubleCount }}
+    </div>
     <NuxtWelcome />
   </div>
 </template>
